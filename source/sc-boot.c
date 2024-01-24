c11902f0b3644abb110fe7e5679ee618c39f2634
ausar <464270342@qq.com>
2024-01-24 20:28:40 +0800

    加入l了补丁
    Signed-off-by: ausar <464270342@qq.com>

--- source/sc-boot.c
+++ source/sc-boot.c
@@ -4,8 +4,13 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
+#include "Save.h"
+#include "EepromSave.h"
+#include "FlashSave.h"
 
+u32 romSize;
 
+bool savingAllowed = true;//有某个游戏是不能开启存档的，否则出错
 // Values for changing mode
 #define SC_MODE_RAM 0x5
 #define SC_MODE_MEDIA 0x3 
@@ -26,6 +31,14 @@ inline void __attribute__((optimize("O0"))) _SC_changeMode(u16 mode) {
 //read: SC_MODE_RAM_RO
 //close:SC_MODE_MEDIA
 
+void pressToContinue(u16 key){
+	u16 pressed = 0;
+	do {
+		VBlankIntrWait();
+		scanKeys();
+		pressed = keysDown();
+	} while (!(pressed & key));
+}
 //---------------------------------------------------------------------------------
 void waitForever() {
 //---------------------------------------------------------------------------------
@@ -70,12 +83,12 @@ int main(void) {
 
 	consoleDemoInit();
 
-	iprintf("Fat32 test\n\n");
+	printf("Fat32 test\n\n");
 
 	if (fatInitDefault()) {
-		iprintf("FAT system initialised\n");
+		printf("FAT system initialised\n");
 	} else {
-		iprintf("FAT system failed!\n");
+		printf("FAT system failed!\n");
 		waitForever();
 	}
 
@@ -83,13 +96,13 @@ int main(void) {
 	char rom_name[] = "sram-zelda-origin.gba";
 	FILE *gba_rom = fopen(rom_name,"rb");
 	if(gba_rom){
-		iprintf("Open OK:%s\n",rom_name);
+		printf("Open OK:%s\n",rom_name);
 	}else{
-		iprintf("GBA open failed:%s\n",rom_name);
+		printf("GBA open failed:%s\n",rom_name);
 		waitForever();
 	}
     fseek(gba_rom, 0, SEEK_END);
-    int romSize = ftell(gba_rom);
+    romSize = ftell(gba_rom);
     fseek(gba_rom, 0, SEEK_SET);
 
     printf("The size of the file is:\n %d bytes\n", romSize);
@@ -97,23 +110,24 @@ int main(void) {
 
     // 拷贝过程
     size_t bytesRead;
-	iprintf("Copying:");
+	printf("Copying:");
 	int offset = 0;
     while ((bytesRead = fread(copyBuf, 1, BUF_LEN, gba_rom)) > 0) {
 		_SC_changeMode(SC_MODE_RAM);
+		#pragma unroll
 		for(int i=0;i<BUF_LEN/4;i++){
 			AGB_ROM[i + offset] = ((u32*)copyBuf)[i];
 		}
 		offset+=BUF_LEN/4;
 		_SC_changeMode(SC_MODE_MEDIA);
-		iprintf("#");
+		printf("#");
     }
 	
     fclose(gba_rom);
 	
 	_SC_changeMode(SC_MODE_RAM);
 	
-    iprintf("File copied successfully.\n Now Patching");
+    printf("File copied successfully.\n Now Patching");
 	u32 entryPoint = *(u32*)0x08000000;
 	entryPoint -= 0xEA000000;
 	entryPoint += 2;
@@ -151,8 +165,20 @@ int main(void) {
 	if (*(u32*)0x08000410 == 0x04000204) {
 		*(vu32*)0x08000410 = 0;
 	}
+	printf("White Screen patch done!\n");
+	
+	///开始给存档打补丁
+
+	
+	const struct save_type_t* saveType = savingAllowed ? save_findTag() : NULL;
+	if (saveType != NULL && saveType->patchFunc != NULL){
+		bool err = saveType->patchFunc(saveType);
+		printf("Save Type Patch Error\n");
+	}
 	
 	_SC_changeMode(SC_MODE_RAM_RO);
+	printf("Press A to Start Game\n");
+	pressToContinue(KEY_A);
 	__asm("swi 0"); // Soft reset
     printf("panic!!.\n");
 	waitForever();
